b1461099f25ebc18df945da8613755e42158e14d
munyuaDeveloper <munyuapeter06@gmail.com>
2022-05-17 13:51:07 +0300

    Created a react netflix clone and hosted it on firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Row from './Row';
+import request from "./requests"
+import Banner from './Banner';
+import Nav from './Nav'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title='NETFLIX ORIGINALS' fetchUrl={request.fetchNetflixOriginals} isLargeRow={true}></Row>
+      <Row title='Trending Now' fetchUrl={request.fetchTrending}></Row>
+      <Row title='Top Rated' fetchUrl={request.fetchTopRated}></Row>
+      <Row title='Action Movies' fetchUrl={request.fetchActionMovies}></Row>
+      <Row title='Comedy Movies' fetchUrl={request.fetchComedyMovies}></Row>
+      <Row title='Horror Movies' fetchUrl={request.fetchHorrorMovies}></Row>
+      <Row title='Romance Movies' fetchUrl={request.fetchRomanceMovies}></Row>
+      <Row title='Documentaries' fetchUrl={request.fetchDocumentaries}></Row>
     </div>
   );
 }
